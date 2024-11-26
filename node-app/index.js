82cbb743d8feae33726d5fe957d874a0fc73ac01
jainayushee <jainnayushee5@gmail.com>
2024-11-26 15:57:35 +0100

    Add

--- node-app/index.js
+++ node-app/index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 const PORT = 80
 app.get('/', (req, res) => {
-    res.send("Goodbddsdsye")
+    res.send("goodbye")
 })
 
 app.listen(PORT, ()=>{
